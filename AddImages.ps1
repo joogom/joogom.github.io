e22e3ea429d01144d7c6f13ee411bb4c09e67353
joogomsnap <joogomsnap@gmail.com>
2024-08-11 23:40:21 +0900

    이미지 파일 이름에서 공백도 제거

--- AddImages.ps1
+++ AddImages.ps1
@@ -4,5 +4,6 @@ Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".jpeg_type1", "" 
 Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".jpeg_type2", "" }
 Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".jpg_type1", "" }
 Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".jpg_type2", "" }
+Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace " ", "" }
 Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace "_", "" }
 (Get-ChildItem -File | Select-Object -ExpandProperty FullName) -join "`r`n" | Set-Clipboard
