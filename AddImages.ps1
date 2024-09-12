9d8fea728baaabb843ba808bf102553a4c0da8a2
joogomsnap <joogomsnap@gmail.com>
2024-09-12 15:20:56 +0900

    "AddImages.ps1" Update

--- AddImages.ps1
+++ AddImages.ps1
@@ -1,9 +1,12 @@
-Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".png_type1", "" }
-Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".png_type2", "" }
-Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".jpeg_type1", "" }
-Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".jpeg_type2", "" }
-Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".jpg_type1", "" }
-Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace ".jpg_type2", "" }
-Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace " ", "" }
-Get-ChildItem -File | Rename-Item -NewName { $_.Name -replace "_", "" }
-(Get-ChildItem -File | Select-Object -ExpandProperty FullName) -join "`r`n" | Set-Clipboard
+# Recursively rename files in all subfolders
+Get-ChildItem -File -Recurse | Rename-Item -NewName { $_.Name -replace ".png_type1", "" }
+Get-ChildItem -File -Recurse | Rename-Item -NewName { $_.Name -replace ".png_type2", "" }
+Get-ChildItem -File -Recurse | Rename-Item -NewName { $_.Name -replace ".jpeg_type1", "" }
+Get-ChildItem -File -Recurse | Rename-Item -NewName { $_.Name -replace ".jpeg_type2", "" }
+Get-ChildItem -File -Recurse | Rename-Item -NewName { $_.Name -replace ".jpg_type1", "" }
+Get-ChildItem -File -Recurse | Rename-Item -NewName { $_.Name -replace ".jpg_type2", "" }
+Get-ChildItem -File -Recurse | Rename-Item -NewName { $_.Name -replace " ", "" }
+Get-ChildItem -File -Recurse | Rename-Item -NewName { $_.Name -replace "_", "" }
+
+# Copy the full paths of all files to the clipboard
+(Get-ChildItem -File -Recurse | Select-Object -ExpandProperty FullName) -join "`r`n" | Set-Clipboard
